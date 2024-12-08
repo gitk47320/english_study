6516a0fdb2b27fce2e43f34c1728678ecb88c552
junya <junya@jenoMacBook-Air.local>
2024-12-08 17:26:59 +0900

    新規追加、編集画面など

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'tops#index'
-  resources :sentences
+  resources :sentences, only: [:index, :new, :create, :show, :edit, :update]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
