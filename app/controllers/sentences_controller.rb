a66c49fc63692e2e9f5aabc03b9b3189474d4104
gitk47320 <34836412+gitk47320@users.noreply.github.com>
2024-12-14 13:44:09 +0900

    Feature/sentences edit create (#2)
    * 新規追加、編集画面など
    
    * pagination
    
    ---------
    
    Co-authored-by: junya <junya@jenoMacBook-Air.local>

--- app/controllers/sentences_controller.rb
+++ app/controllers/sentences_controller.rb
@@ -1,8 +1,42 @@
 class SentencesController < ApplicationController
+  before_action :set_sentence, only: [:show, :edit, :update, :destroy]
+
   def index
-    @sentences = Sentence.all
+    @sentences = Sentence.all.order(:id).page(params[:page])
+  end
+
+  def new
+    @sentence = Sentence.new
+  end
+
+  def edit
+  end
+
+  def update
+    if @sentence.update(sentence_params)
+      redirect_to sentences_path, notice: 'Sentence was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def create
+    @sentence = Sentence.new(sentence_params)
+
+    if @sentence.save
+      redirect_to @sentence, notice: '登録成功しました。'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def sentence_params
+    params.require(:sentence).permit(:correct_english, :incorrect_english, :japanese)
+  end
+  
+  def set_sentence
+    @sentence = Sentence.find(params[:id])
   end
 end
--- app/models/sentence.rb
+++ app/models/sentence.rb
@@ -1,2 +1,12 @@
 class Sentence < ApplicationRecord
+  VALID_ENGLISH_FORMAT = /\A[a-zA-Z0-9\s.,'"']*\z/
+  ENGLISH_FORM_ERROR = '半角英数字、半角スペース、ピリオド、カンマ、引用符のみ入力可能です'
+  validates :correct_english, 
+            presence: true, 
+            format: { with: VALID_ENGLISH_FORMAT, 
+                      message: ENGLISH_FORM_ERROR }
+  validates :incorrect_english, 
+            presence: true,
+            format: { with: VALID_ENGLISH_FORMAT, 
+                      message: ENGLISH_FORM_ERROR }
 end
